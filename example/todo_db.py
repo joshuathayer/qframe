b807f9228fb3046e4173bc4313e73d46b62acf93
Joshua Thayer <joshuathayer@riseup.net>
2018-12-14 01:27:12 -0800

    README

---
+++ example/todo_db.py
@@ -0,0 +1,6 @@
+app_state = {'todos': [],
+             'new-todo-text': "",
+             'todo-id': 0}
+
+subscriptions = {'todos': ['todos'],
+                 'new-todo-text': ['new-todo-text']}
